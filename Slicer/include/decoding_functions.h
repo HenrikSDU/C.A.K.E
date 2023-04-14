21e49d1e86e1dd3183ff9a5109dd424a351a2c64
Botond Bencsik <botibencsik@gmail.com>
2023-04-14 22:15:10 +0200

    very big progress with search functions

--- Slicer/include/decoding_functions.h
+++ Slicer/include/decoding_functions.h
@@ -1,5 +1,11 @@
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    double x;
+    double y;
+} point_t;
 
 double cubic_bezier_x(double start_x, double c1_x, double c2_x, double end_x, double t) {
     double ratio = (double)1 - t;
@@ -13,10 +19,16 @@ double cubic_bezier_y(double start_y, double c1_y, double c2_y, double end_y, do
     return y;
 }
 
+
+/* Small example for the str_shift_left function
+    char test1[] = "asd";
+    str_shift_left(test1, sizeof(test1), 'b');
+    printf("Asd: %s\n", test1);
+    this will print "bas" */ 
 void str_shift_left(char* str, int length, char next) {
     char* temp = malloc((length - 1) * sizeof(char));
     for(int i = 1; i < length - 1 + 1; i++) {
-        temp[i] = str[i - 1]; // temp = _asd
+        temp[i] = str[i - 1]; // temp = _as
     }
     temp[0] = next;
     for(int i = 0; i < length - 1; i++) {
@@ -24,4 +36,61 @@ void str_shift_left(char* str, int length, char next) {
     }
 
     free(temp);
+}
+
+unsigned int str_find_d(char* str, unsigned int cursor, unsigned int length) {
+    char d[4]; //The string we want to find, has to be inputted backwards except for the NULL char
+    d[0] = 34; // "
+    d[1] = 61; // =
+    d[2] = 100; // d
+    d[3] = 0; // NULL
+    char temp[4];
+    unsigned int match = 0; //Counts how many matches there are between the strings
+
+    for(int i = cursor; i < length; i++) {
+        str_shift_left(temp, sizeof(temp), str[i]);
+        //printf("%s\n", temp);
+        match = 0;
+        for(int j = 0; j < sizeof(d); j++) {
+            //printf("the compared chars: %c, %c\n", d[j], temp[j]);
+            if(d[j] == temp[j])
+                match++;
+        }
+        //printf("match is: %d\n", match);
+        if(match == 4)
+            return i + 1; // Returns the current "position" basically where the specified string ends
+        }
+    return length; //Error return is yet to be decided
+}
+/* Todo: str_find functions for width and height*/
+
+point_t get_coords(char* str, unsigned int* cursor) {
+    point_t point;
+    char coord[10]; // 10 for now as that should be enough for every number however more logic can be added to set a resolution for floats
+    int count = 0;
+    while(str[*cursor] > 47 || str[*cursor] < 58) {
+        coord[count] = str[*cursor];
+        count++;
+        *cursor++;
+    }
+    point.x = atof(coord);
+
+    memset(coord, 0, sizeof(char) * sizeof(coord));
+    count = 0;
+
+    while(str[*cursor] > 47 || str[*cursor] < 58) {
+        coord[count] = str[*cursor];
+        count++;
+        *cursor++;
+    }
+    point.y = atof(coord);
+
+    return point;
+}
+
+point_t init_point(void) {
+    point_t point;
+    point.x = 0.0;
+    point.y = 0.0;
+    return point;
 }
--- Slicer/main.c
+++ Slicer/main.c
@@ -9,64 +9,120 @@
 
 #define START_OF_SVG 39 // The first line of the file is not useful here therefore skipping over it
 #define WIDTH_POS 51 // This is where the value of the width of the picture is stored
-#define W_H_DELTA 13 // The difference between the end of the width value and the beginning of the height
+#define WH_DELTA 13 // The difference between the end of the width value and the beginning of the height
 
+/* Global variables to keep track of current position in the coordinate system and in the string  */
+unsigned int current_x = 0, current_y = 0, cursor = 0, prev_cursor;
+
+
+
+/* The idea of this is to use this enum as a more descriptive flag because it will have the d variant as a value when it detects the secon = sign */
+enum path_line {
+    null,
+    stroke,
+    _d,
+};
 
 
 int main() {
     /* fp stands for file pointer, and is of type file(FILE) pointer(*) */
-    FILE* fp = fopen("test14.svg", "r"); // fopen will get the address of a file, and open it in a mode, here read
+    FILE* fp = fopen("test13.svg", "r"); // fopen will get the address of a file, and open it in a mode, here read
+    FILE* gcode = fopen("test14.gcode", "w");
 
     unsigned int width, height;
+    enum path_line obj = null;
+    point_t current_pos = init_point();
 
     /* Determining the lenght of a file, not sure if this is the best way but it works */
     fseek(fp, 0, SEEK_END); // Putting cursor at the end
     int length = ftell(fp); // Getting cursor location (distance from the start of the file)
     printf("Length of file in bytes: %d\n", length);
 
-    /* Small little function to determine the width and height indicated in the svg */
+    fseek(fp, 0, SEEK_SET);
+    char* svg = malloc(length * sizeof(char) + 1);
+    for(int i = 0; i < length; i++) {
+        svg[i] = getc(fp);
+    }
+    svg[length] = 0;
+    printf("%s\n", svg);
+
+    /* Debug operations */
+    /*
+    for(int i = 0; i <= length; i++) {
+        printf("Index: %d, Char: %d\n", i, svg[i]);
+    }
+    printf("\n");
+    */
+
+    // Todo: make str_find functions for these two aswell
+    // Filtering out the width and height info
     {
         char str[4];
-        fseek(fp, WIDTH_POS, SEEK_SET);
-        for(int i = 0; i < 4; i++) {
-            char temp_ch = getc(fp);
-            printf("%d\n", temp_ch);
-            if(temp_ch == 34) {
-                break;
-            }
-            str[i] = temp_ch;
+        for(int i = 0; i < sizeof(str); i++) {
+            str[i] = svg[i + WIDTH_POS];
+            cursor = i + WIDTH_POS;
         }
-        printf("Str: %s\n", str);
         width = atoi(str);
-        printf("Width: %d\n", width);
 
-        memset(str, 0, sizeof(str)); // Resetting the string, there was an issue with the strings but this fixed it
-
-        while(getc(fp) != 34);
-
-        for(int i = 0; i < 4; i++) {
-            char temp_ch = getc(fp);
-            printf("%d\n", temp_ch);
-            if(temp_ch > 57 ||temp_ch < 48) {
-                break;
-            }
-            str[i] = temp_ch;
+        while(svg[cursor] != '=') {
+            cursor++;
         }
-        printf("Str: %s\n", str);
-        height = atoi(str);
-        printf("Height: %d\n", height);
-    }
-    
-    // Small testing for the str_shift_left function
-    char test1[] = "asd";
-    str_shift_left(test1, sizeof(test1), 'b');
-    printf("Asd: %s\n %d %d %d %d", test1, test1[0], test1[1], test1[2], test1[3]);
 
-   
+        cursor += 2;
+        for(int i = 0; i < sizeof(str); i++) {
+            str[i] = svg[i + cursor];
+        }
+        height = atoi(str);
+    }
+    printf("Width: %d, Height: %d\n", width, height);
+
+    // If more colors or layers are wanted on the same picture the stroke property could be useful and should be checked before the coordinates
+
+    /* Temporary file for easier conversion */
+    FILE* temp_d = fopen("temp_d.txt", "w");
+
+    while(svg[cursor] != 0) {
+        cursor = str_find_d(svg, cursor, length); // Will return cursor location where it finds " "=d " or if it doesn`t then the length that was passed in as an argument
+        printf("Cursor: %d\n", cursor);
+        if(cursor == length) // If cursor is at the end then terminate the loop
+            break;
+
+        for(int i = cursor; i < length; i++) {
+            // Bad practice but many if statements to decide how to print the current char, new lines chars and numbers matter
+            if((svg[i] == 'M' || svg[i] == 'C' || svg[i] == 'L') && i == cursor) { // If letter and first char then no new line in the beginning
+                fprintf(temp_d, "%c" , svg[i]);
+                fprintf(temp_d, "\n");
+            }
+            else if((svg[i] == 'M' || svg[i] == 'C' || svg[i] == 'L') && svg[i - 1] < 65) { // If previous character was a number start in new line
+                fprintf(temp_d, "\n");
+                fprintf(temp_d, "%c" , svg[i]);
+                fprintf(temp_d, "\n");
+            }
+            else if((svg[i] > 47 && svg[i] < 58) || svg[i] == 46) { // If number or . then write those in one line
+                fprintf(temp_d, "%c" , svg[i]);
+            }
+            else if(svg[i] == 32) { // Putting \n instead of space
+                fprintf(temp_d, "\n");
+            }
+            if(svg[i] == 34) // " is the terminating character for the d variable
+                break;
+        }
+        fprintf(temp_d, "\n\n\n"); // Separation between objects
+    }
+
+    // I can do this but it`s quite pointless at this time
+    fprintf(stderr, "gddf");
+
+
+    printf("\nThe remaining characters\n");
+    while(cursor < length) {
+        printf("%c", svg[cursor]);
+        cursor++;
+    }
 
     fclose(fp);
+    free(svg);
+    //free(d);
 
     return 0;
 }
-
-// i have to learn about pipes now if i dont want to have a temporary file ehhhhhhhhhh
