09e86f6462491665a1a37102795ca04a285b4fb3
Botond Bencsik <botibencsik@gmail.com>
2023-04-30 20:37:51 +0200

    functional but unlinked gui

--- Slicer/gui/lib/main.dart
+++ Slicer/gui/lib/main.dart
@@ -1,264 +1,73 @@
-import 'package:english_words/english_words.dart';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:file_picker/file_picker.dart';
-import 'dart:io';
 
 void main() {
-  runApp(MyApp());
+  runApp(Slicer());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class Slicer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
+  _SlicerState createState() => _SlicerState();
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+class _SlicerState extends State<Slicer> {
+  String? filepath = " ";
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
+  Future<String?> getFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
 
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if(favorites.contains(current)) {
-      favorites.remove(current);
-      print("-$current");
-    } else {
-      favorites.add(current);
-      print("+$current");
-    }
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-      switch (selectedIndex) {
-        case 0:
-          page = GeneratorPage();
-          break;
-        case 1:
-          page = FavoritesPage();
-          break;
-        default:
-          throw UnimplementedError('no widget for $selectedIndex');
+    if(result != null) {
+      String? path = result.files.single.path;
+      if(path != null) {
+        print(path);
+        
+        return path;
+      } else {
+        print("There was an error with the file selection!  path = null");
+        return null;
       }
+    } else {
+      print("User cancelled the process");
+      return null;
+    }
+  }
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "slicer",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Image slicing'),
+                const SizedBox(height: 5),
+                const Text('Accepted format(s): *.bmp'),
+                const SizedBox(height: 5),
+                ElevatedButton(
+                  onPressed: () async {
+                    String? path = await getFile();
+                    print("path is $filepath");
                     setState(() {
-                      selectedIndex = value;
+                      filepath = path;
                     });
                   },
+                  child: const Text("Select file"),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
+                Text('Selected file: $filepath'),
+              ]
+            ),
           ),
-        );
-      }
-    );
-  }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
+        )
       ),
     );
   }
 }
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onSecondary,
-      fontSize: 40,
-    );
-
-    if(appState.favorites.isEmpty) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("You a have no favorites yet!", style: style),
-          ],
-        ),
-      );
-    } else {
-
-
-      return ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text("You have ${appState.favorites.length} favorite(s)"),
-          ),
-          for(var item in appState.favorites)
-            ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text(item.asLowerCase),
-            )
-        ],
-      );
-    }
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-      fontSize: 55,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-
-        child: Text(pair.asLowerCase, 
-        style: style, 
-        semanticsLabel: "${pair.first} ${pair.second}"),
-      ),
-    );
-  }
-}
-
-class SmallCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-      fontSize: 25,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(padding: const EdgeInsets.all(8.0),
-        child: Text("Favorites", style: style),
-      )
-    );
-  }
-}
-/*
-ElevatedButton(onPressed: () async {
-                    FilePickerResult? result = await FilePicker.platform.pickFiles();
-
-                    if (result != null) {
-                      String? path = result.files.single.path;
-                      if (path != null) {
-                        File file = File(path);
-                        print(path);
-                        print(file);
-
-                      } else {
-                      // Handle the case where the path is null
-                      }
-                      } else {
-                      // User canceled the picker
-                      }
-            }, 
-            child: 
-            Text("Choose a file")),
-            */
