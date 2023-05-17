3b9eeaca4beefd0223358bfe7a98ec7dde1eda33
Botond Bencsik <botibencsik@gmail.com>
2023-05-17 16:06:11 +0200

    Formatting for nicer looks

--- Firmware/CAKE-Communication/src/main.c
+++ Firmware/CAKE-Communication/src/main.c
@@ -42,9 +42,9 @@ volatile char* file; //saves the incoming bytes from the computer
 
 CAKEFILE cakefile; //contains an array of instructions and points (table_instruction(s)) and an array that indicates whether the data saved at a specific index is a coordinate or an extruder instruction
 
-ISR(USART_RX_vect){
+ISR(USART_RX_vect) {
     
-    switch(phase){
+    switch(phase) {
 
         case upload://in the upload phase the incoming bytes are stored in the file array (a simple array of chars)
             file[file_index] = UDR0;
@@ -65,98 +65,97 @@ ISR(USART_RX_vect){
     
 }
 
-ISR(PCINT1_vect){
+ISR(PCINT1_vect) {
 
     if((PINC & (1 << BUTTON4)) == 0)
-        PORTB ^= (1<<PB5);
-        //printf("hey");
+        PORTB ^= (1 << PB5);
 
 }
 
 int main(void) { 
 
-    i2c_init();//initialize I2C communication
+    i2c_init();// Initialization of the I2C communication
     
-    //LCD_init();//initialize the LCD
-    lm75_init();//initialize the temperature sensor
+    //LCD_init();// Initialization of the LCD
+    //lm75_init();// Initialization of the temperature sensor
     
     uart_init();
     io_redirect();
 
-    //configuration of the IO pins
+    // Configuration of the IO pins
 
-    DDRC |= 0x30; //for I2C
+    DDRC |= 0x30; // For I2C
     PORTC |= 0x30;
-    DDRB |= (1<<PB5);
+    DDRB |= (1 << PB5);
 
    
-    //enable RX interrupt
+    // Enabling RX interrupt
     UCSR0B |= (1 << RXCIE0);
-    sei(); //enable interrupts globaly
+    sei(); // Enable interrupts globaly
 
-    while(phase == memory_init){
+    while(phase == memory_init) {
        
-        if(readcycle_complete){//handle memoryinitflags if recievecycle is complete
+        if(readcycle_complete) { // Handle memoryinitflags if recievecycle is complete
             
             //PORTD |= (1 << PD7); //indicate that recievecycle is complete
 
-            filesize = memory_init_flags[0] * 255 + memory_init_flags[1]; //compute filesize
+            filesize = memory_init_flags[0] * 255 + memory_init_flags[1]; // Compute filesize
 
-            if(filesize > 0){ //here size constraint possible
-                file = (char*)malloc(filesize*sizeof(unsigned char)); //allocate memory for incoming CAKE-file
+            if(filesize > 0) { // Here size constraint possible
+                file = (char*)malloc(filesize * sizeof(unsigned char)); // Allocate memory for incoming CAKE-file
                 
-                if(file != NULL){
-                    //PORTD |= (1 << PD6); //indicate successful memory allocation
+                if(file != NULL) {
+                    //PORTD |= (1 << PD6); // Indicate successful memory allocation
                 }
                 
             }
 
-            instruction_count = memory_init_flags[2]; //getting the amount of instructions
+            instruction_count = memory_init_flags[2]; // Getting the amount of instructions
 
                 cakefile.path = (table_instruction*)calloc(instruction_count * sizeof(table_instruction), sizeof(table_instruction));
                 cakefile.instruction_locations = (bool*)calloc(instruction_count * sizeof(bool), sizeof(bool));
 
-            //sending feedback 
+            // Sending feedback 
             for(unsigned char j = 0; j < 10; j++)
                 usart_send(memory_init_flags[j]);
 
-            //resetting readcycle complete flag
+            // Resetting readcycle complete flag
             readcycle_complete = false;
 
             interrupt_count = 0;
 
-            //going over to next phase
+            // Going over to next phase
             phase = upload;
         }
         
     }
     
 
-    while(phase == upload){
+    while(phase == upload) {
         int k;
 
-        if((interrupt_count) >= filesize){//checking for successful upload
+        if((interrupt_count) >= filesize) { // Checking for successful upload
 
-            interrupt_count = 0; //resetting i
+            interrupt_count = 0; // Resetting interrupt count
             //PORTD |= (1 << PD4);
             
-            for(k = 0; k < filesize; k++)//send file back for feedback
+            for(k = 0; k < filesize; k++) // Send file back for feedback
                 usart_send(file[k]);
 
-            UCSR0B &= ~(1 << RXCIE0);//disable rx-interrupt
+            UCSR0B &= ~(1 << RXCIE0); // Disable rx-interrupt
 
-            file_processing(&cakefile, file, filesize);//proccessing the recieved array
+            file_processing(&cakefile, file, filesize); // Proccessing the recieved array
 
             LCD_init();
             LCD_set_cursor(0,0);
             printf("FS:%d", filesize);
-            for(k = 0; k < instruction_count; k++){
+            for(k = 0; k < instruction_count; k++) {
 
                 LCD_set_cursor(0, (k%4));
-                if(cakefile.instruction_locations[k] == 1){
-                    printf("G%d", cakefile.path[k].extruder_inst+1);
+                if(cakefile.instruction_locations[k] == 1) {
+                    printf("G%d", cakefile.path[k].extruder_inst + 1);
                 }
-                else{
+                else {
                     printf("X:%dY:%d ", cakefile.path[k].table_coord.x, cakefile.path[k].table_coord.y);
                 }
                 _delay_ms(10);
@@ -171,14 +170,14 @@ int main(void) {
 
     }
 
-    while(phase == main_operation){
+    while(phase == main_operation) {
         
         button_init();
         LCD_init();
         LCD_set_cursor(0,0);
 
 
-        //Init for IOBoard - speed measurement
+        // Initialization for IOBoard - speed measurement
         
         DDRB |= (1<<PB5);
 
@@ -191,29 +190,29 @@ int main(void) {
 
         printf("B3:setB4:incB5:decB6:dich");
         char direction;
-        while(1){
+        while(1) {
 
-            if((PINC & (1 << BUTTON3)) == 0){
+            if((PINC & (1 << BUTTON3)) == 0) {
 
                 PWM_T0A_set(desired_PWM);
                 
                 _delay_ms(200);
             }
 
-            if((PINC & (1 << BUTTON4)) == 0){
+            if((PINC & (1 << BUTTON4)) == 0) {
                 desired_PWM += 10;
                 LCD_set_cursor(0,2);
                 printf("desPWM: %u ", desired_PWM);
                 _delay_ms(200);
             }
-            if((PINC & (1 << BUTTON5)) == 0){
+            if((PINC & (1 << BUTTON5)) == 0) {
 
                 desired_PWM -= 10;
                 LCD_set_cursor(0,2);
                 printf("desPWM: %u ", desired_PWM);
                 _delay_ms(200);
             }
-            if((PINC & (1 << BUTTON6)) == 0){
+            if((PINC & (1 << BUTTON6)) == 0) {
                 direction = 0b00000001 & direction;
                 PWM_T0A_direction_change(direction);
                 LCD_set_cursor(0,3);
@@ -233,7 +232,7 @@ int main(void) {
 }
 
 
-void usart_send(unsigned char data){ 
+void usart_send(unsigned char data) { 
     //check whether there is space in the sending buffer
     while(!(UCSR0A & (1 << UDRE0)));//wait for transmit buffer
     UDR0 = data;
